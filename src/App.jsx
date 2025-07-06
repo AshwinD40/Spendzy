7aa49ee0fdd22bbdaae8da615f94e0eec40b0cbb
Ashwin Chaudhary <ashwinkumarchaudhary950@gmail.com>
2025-07-06 15:02:56 +0530

    account unsubscribe fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useNavigate, } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Signup from './pages/Signup'
 import { Toaster } from 'react-hot-toast'  
+import { useEffect } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
 
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if(!user){
+        navigate("/");
+      }
+    })
+
+    return () => unsubscribe();
+  }, [navigate]);
 
   return (
   <>
